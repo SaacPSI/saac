5c6b2ecd251857851914c6712a3644f44ec019d0
aurelien.milliat <aurelien.milliat@imt-atlantique.fr>
2025-12-11 09:53:12 +0100

    Adding PsiStudioExtension project.

---
+++ Components/PsiStudioReplayExtension/src/PsiStudioNetworkInfo.cs
@@ -0,0 +1,55 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace Microsoft.Psi.PsiStudio
+{
+    /// <summary>
+    /// Message class message from <see cref="NetworkStreamsManager"/>.
+    /// </summary>
+    public class PsiStudioNetworkInfo
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PsiStudioNetworkInfo"/> class.
+        /// </summary>
+        /// <param name="evt">State of PsiStudio.</param>
+        /// <param name="interval">Current Interval of playback.</param>
+        /// <param name="sessionName">Name of the surrent session.</param>
+        public PsiStudioNetworkInfo(PsiStudioNetworkEvent evt, TimeInterval? interval = null, string sessionName = null)
+        {
+            this.Event = evt;
+            this.SessionName = sessionName;
+            this.Interval = interval ?? TimeInterval.Infinite;
+        }
+
+        /// <summary>
+        /// Enum providing the state of PsiStudio.
+        /// </summary>
+        public enum PsiStudioNetworkEvent
+        {
+            /// <summary>
+            /// PsiStudio is playing in playback so the stream will start shortly.
+            /// </summary>
+            Playing,
+
+            /// <summary>
+            /// PsiStudio is stoping in playback no more data incoming.
+            /// </summary>
+            Stoping,
+        }
+
+        /// <summary>
+        /// Gets the PsiTudioEvent instance.
+        /// </summary>
+        public PsiStudioNetworkEvent Event { get; private set; }
+
+        /// <summary>
+        /// Gets the current session name.
+        /// </summary>
+        public string SessionName { get; private set; }
+
+        /// <summary>
+        /// Gets the current timeInterval for the playback.
+        /// </summary>
+        public TimeInterval Interval { get; private set; }
+    }
+}
